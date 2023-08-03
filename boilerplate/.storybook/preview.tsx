a287d0465ae5b523b8ddc3badd188f1c0c52bfd6
Joao Costa <joaopedrocosta.contato@gmail.com>
2023-08-03 00:04:10 -0300

    (boilerplate) quick fixes

--- boilerplate/.storybook/preview.tsx
+++ boilerplate/.storybook/preview.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { themes } from '@storybook/theming';
+
 import type { Preview } from '@storybook/react'
 import GlobalStyles from '../src/styles/global'
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+      default: 'dark'
+    },
+    docs: {
+      theme: themes.light
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
