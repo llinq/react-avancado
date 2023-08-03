2aef7e8afe01b4d5ed7b152c6014b69c092be2cb
Joao Costa <joaopedrocosta.contato@gmail.com>
2023-08-02 23:19:03 -0300

    (client) create client project based on boilerplate

---
+++ client/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
+  stories: ['../src/components/**/stories.tsx', '../src/components/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  staticDirs: ['../public']
+}
+export default config
---
+++ client/next.config.js
@@ -0,0 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  compiler: {
+    styledComponents: true
+  }
+})
+
+module.exports = nextConfig
