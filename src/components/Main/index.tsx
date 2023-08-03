8b336ec44a1a7e707e84d8f94c526d29e228ad82
Joao Costa <joaopedrocosta.contato@gmail.com>
2023-08-03 00:33:02 -0300

    (draft) aaaa

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
+  title = 'React Avançado :)',
   description = 'Typescript, ReactJS, NextJS e Styled Components'
 }) => (
   <S.Wrapper>
